c0183f32b1f70cffb523b0df6264681a95173678
jcpit <jake@jcpit.com.au>
2024-12-13 11:53:20 +1100

    Update update.php
    Use domain_id instead

--- api/v1/domains/update.php
+++ api/v1/domains/update.php
@@ -6,21 +6,17 @@ require_once '../validate_api_key.php';
 // Enforce a POST request
 require_once '../require_post_method.php';
 
-// Get the domain name from the request
-$domain_name = mysqli_real_escape_string($mysqli, $_POST['domain_name']);
+// Parse ID
+$domain_id = intval($_POST['domain_id']);
 
 // Default
 $update_count = false;
 
-// Ensure the domain name is provided
-if (!empty($domain_name)) {
-    // Fetch the domain based on the name and client_id
-    $domain_row = mysqli_fetch_assoc(
-        mysqli_query(
-            $mysqli,
-            "SELECT * FROM domains WHERE domain_name = '$domain_name' AND domain_client_id = $client_id LIMIT 1"
-        )
-    );
+// Check if domain ID is valid and belongs to the client
+if (!empty($domain_id)) {
+
+    // Fetch the current domain record
+    $domain_row = mysqli_fetch_assoc(mysqli_query($mysqli, "SELECT * FROM domains WHERE domain_id = '$domain_id' AND domain_client_id = $client_id LIMIT 1"));
 
     if ($domain_row) {
         // Variable assignment from POST - assigning the current database value if a value is not provided
@@ -29,10 +25,8 @@ if (!empty($domain_name)) {
         $status = isset($_POST['status']) ? mysqli_real_escape_string($mysqli, $_POST['status']) : $domain_row['domain_status'];
         $notes = isset($_POST['notes']) ? mysqli_real_escape_string($mysqli, $_POST['notes']) : $domain_row['domain_notes'];
 
-        // Update query
-        $update_sql = mysqli_query(
-            $mysqli,
-            "
+        // Update Query
+        $update_sql = mysqli_query($mysqli, "
             UPDATE domains 
             SET 
                 domain_name = '$name',
@@ -40,11 +34,10 @@ if (!empty($domain_name)) {
                 domain_status = '$status',
                 domain_notes = '$notes'
             WHERE 
-                domain_name = '$domain_name' 
-                AND domain_client_id = $client_id
+                domain_id = $domain_id 
+                AND domain_client_id = $client_id 
             LIMIT 1
-        "
-        );
+        ");
 
         // Check if the update was successful
         if ($update_sql) {
@@ -54,9 +47,6 @@ if (!empty($domain_name)) {
             logAction("Domain", "Edit", "$name updated via API ($api_key_name)", $client_id);
             logAction("API", "Success", "Edited domain $name via API ($api_key_name)", $client_id);
         }
-    } else {
-        // Log a failed attempt to find the domain
-        logAction("API", "Failure", "Attempted to edit domain $domain_name via API ($api_key_name), but it was not found", $client_id);
     }
 }
 
